96eec8e6b2640ecf8646c14bd19a8463b3d9bdbf
Carlos Misachi <calphonce31@gmail.com>
2022-05-23 19:10:49 +0300

    resetPass

---
+++ js/resetPass.js
@@ -0,0 +1,19 @@
+"use strict";
+
+window.oncontextmenu =()=>{
+    return false;
+}
+
+document.getElementById("login").onclick =()=>{
+    let email = document.getElementById("email").value;
+
+    firebase.auth().sendPasswordResetEmail(email)
+    .then(() =>{
+        alert("A Reset Link Has Been Sent To Your Email");
+        alert("Please login with your new password");
+        window.location.href="/html/index.html";
+    }).catch((error)=>{
+        alert(error.message);
+    })
+
+}
