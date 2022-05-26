ae01e0b72b34cda3ba31f696ce75fc8865cf9d6c
Carlos Misachi <calphonce31@gmail.com>
2022-05-26 17:27:16 +0300

    updates

--- js/allTeachers.js
+++ js/allTeachers.js
@@ -12,6 +12,10 @@ firebase.auth().onAuthStateChanged((user)=>{
                 let name = doc.data().name;
                 let staffNo = doc.data().staffNo;
                 let subjects = doc.data().subjects;
+                let teacherDocId = doc.data().teacherDocId;
+                let teacherUserId = doc.data().teacherUserId;
+
+                let carriedId = teacherDocId +"?"+ teacherUserId;
 
 
                 let content ="";
@@ -20,16 +24,95 @@ firebase.auth().onAuthStateChanged((user)=>{
                 content +=`<td colspan="2">${name}</td>`
                 content +=`<td>${staffNo}</td>`
                 content +=`<td>${subjects}</td>`
-                content +=`<td><button>more</button></td>`
-                content +=`<td><button>edit</button></td>`
+                content +=`<td><button onClick="viewMore(\`${teacherDocId}\`)" data-bs-toggle="modal" data-bs-target="#teacherDetails" class="btn btn-outline-success">more</button></td>`
+                content +=`<td><button onClick="editTeacher(\`${carriedId}\`)" data-bs-toggle="modal" data-bs-target="#editTeacherDetails" class="btn btn-outline-success">edit</button></td>`
                 content +=`<td><i class="fa-solid fa-trash-can"></i></td>`
               content +=`</tr>`
 
               $("#teacherData").append(content);
+            })
+        })
+        // viewMore Function
+        window.viewMore = (value)=>{
+            // alert(value);
+            firebase.firestore().collection("teachers").doc(value)
+            .get().then((doc)=>{
+                let name = doc.data().name;
+                let email = doc.data().email;
+                let phoneNumber = doc.data().phoneNum;
+
+                document.getElementById("name").innerHTML = name;
+                document.getElementById("phoneNumber").innerHTML = phoneNumber;
+                document.getElementById("email").innerHTML = email;
             }).catch((error)=>{
                 alert(error.message);
             })
-        })
+        }
+        // Edit Teacher
+
+        window.editTeacher =(value)=>{
+            // alert(value);
+            let result = value.split("?");
+            let teacherDocId = result[0];
+            let teacherUserId = result[1];
+
+            firebase.firestore().collection("teachers").doc(teacherDocId).get()
+            .then((doc)=>{
+                
+                    let name = doc.data().name;
+                    let staffNo = doc.data().staffNo;
+                    let subjects = doc.data().subjects;
+                    let email = doc.data().email;
+                    let phoneNumber = doc.data().phoneNum;
+
+                    document.getElementById("teacherName").value = name;
+                    document.getElementById("staffNo").value = staffNo;
+                    document.getElementById("subjects").value = subjects;
+                    document.getElementById("tPhone").value = phoneNumber;
+                    document.getElementById("tEmail").value = email;
+                
+                
+            })
+
+            // updating
+            document.getElementById("updateButton").onclick =()=>{
+ 
+                let name = document.getElementById("teacherName").value; 
+                let staffNo = document.getElementById("staffNo").value; 
+                let subjects = document.getElementById("subjects").value;
+               let phoneNum = document.getElementById("tPhone").value;
+                let email = document.getElementById("tEmail").value;
+
+                firebase.firestore().collection("teachers").doc(teacherDocId)
+                .update({
+                    name: name,
+                    staffNo:staffNo,
+                    subjects: subjects,
+                    phoneNumber:phoneNum,
+                    email: email
+
+                }).then(()=>{
+                    alert("teacher updated");
+
+                    firebase.firestore().collection("users").doc(teacherUserId)
+                    .update({
+                        name:name
+                    }).then(()=>{
+                        alert("user updated");
+                        window.location.reload();
+                    }).catch((error)=>{
+                        alert(error.message);
+                    })
+
+                }).catch((error)=>{
+                    alert(error.message);
+                })
+
+
+
+            }
+
+        }
 
 
     }else{
